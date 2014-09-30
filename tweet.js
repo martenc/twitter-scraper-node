3d11e6d5c8ae4095cd940bddc65a86f0873dc633
Marten T. Compoc <martenc@gmail.com>
2014-09-30 16:21:24 -0700

    broke out fields

--- tweet.js
+++ tweet.js
@@ -3,7 +3,16 @@ var mongoose = require('mongoose');
 // create a user model
 var Tweet = mongoose.model('Tweet', {
   //twitterhandle: String,
+  twitterId: String,
+  twitterIdStr: String,
   tweet: String,
+  source: String,
+  name: String,
+  screen_name: String,
+  retweet_count: String,
+  entities: String,
+  favorited: String,
+  retweeted: String,
   created: Date
 });
 
